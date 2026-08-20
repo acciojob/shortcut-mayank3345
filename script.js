ba3053fa77856bfc4079d13407d184fe34f756e3
Aditya Singh <102604262+mayank3345@users.noreply.github.com>
2026-08-20 19:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function shortcut(s1, s2) {
-  // your code here
+	return [s1[0]+s2[0]];
+	
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+//Do not change the code below.
+var s1 = prompt("Enter s1:");
+var s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
+
